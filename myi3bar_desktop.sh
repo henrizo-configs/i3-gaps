17f845c86fdec8da6fb319423bb234453bbf6a90
matheushsouza <matheus.hs97@gmail.com>
2020-01-12 21:14:53 -0300

    Rework.

---
+++ myi3bar_desktop.sh
@@ -0,0 +1,91 @@
+#! bin/bash
+
+# DEPENDENCY: icons depends on font awesome and typicons
+echo '{ "version": 1 }'
+# i3 protocol stuff
+echo '['
+echo '[]'
+
+# static auxiliary variables
+defaultColour="#b39478"
+bloodyRed="#db4b4b"
+
+# Output blocks loop
+while :;
+do
+    # updating variables
+    # keyboard info
+    keyboardLayout=$(setxkbmap -query | grep layout | rev | cut -c 1,2 | rev)
+    
+    # date info
+    date=$(date "+%a, %d %b %Y")
+
+    #time info
+    time=$(date "+%T")
+    
+    # audio info
+    audioState=$(amixer get Master  | grep -Eo "\[(off|on)\]" | sed -n '1,1 p')
+    masterLevel=$(amixer get Master | grep -Eo "[0-9]+%"      | sed -n '1,1 p')
+    if [[ $audioState == "[off]" ]]
+    then
+        audioIcon=""
+        audioColour=$bloodyRed
+    else
+        audioIcon=""
+        audioColour=$defaultColour
+    fi
+
+    # mpd info
+    nowPlaying=$(mpc current)
+
+    mpdStatus=$(mpc status | grep -Eo "(playing|paused)")
+    if [[ $mpdStatus == "playing" ]]
+    then
+        mpdIcon=""
+    elif [[ $mpdStatus == "paused" ]]
+    then
+        mpdIcon=""
+    else
+        mpdIcon=""
+    fi                    
+    
+    # connection info
+    ethStatus=$(ip link show | grep -Eo "state DOWN")
+    if [[ $ethStatus != "state DOWN" ]]
+    then
+        connectionIcon=""
+        connectionColour=$defaultColour
+        connectionStatus=" connected"
+    else
+        connectionIcon=""
+        connectionColour=$bloodyRed
+        connectionStatus="not connected"
+    fi                    
+
+    # output blocks
+        echo ",[{\"name\"     :\"mpd\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"$mpdIcon  $nowPlaying\"}
+                                        
+              , {\"name\"     :\"masterLevel \"
+              ,  \"color\"    :\"$audioColour\"
+              ,  \"full_text\":\"$audioIcon $masterLevel\"}
+                
+              , {\"name\"     :\"connectionInfo\"
+              ,  \"color\"    :\"$connectionColour\"
+              ,  \"full_text\":\"$connectionIcon  $connectionStatus\"}
+                
+              , {\"name\"     :\"keyboardInfo \"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"  $keyboardLayout\"}
+                
+              , {\"name\"     :\"date\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"  $date\"}
+                
+              , {\"name\"     :\"time\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\" $time\"}
+           ]"
+    sleep 1
+done
---
+++ myi3bar_laptop.sh
@@ -0,0 +1,138 @@
+#! bin/bash
+
+# DEPENDENCY: icons depends on font awesome and typicons
+echo '{ "version": 1 }'
+# i3 protocol stuff
+echo '['
+echo '[]'
+
+# static auxiliary variables
+defaultColour="#b39478"
+bloodyRed="#db4b4b"
+
+# Output blocks loop
+while :;
+do
+    # updating variables
+    # keyboard info
+    keyboardLayout=$(setxkbmap -query | grep layout | rev | cut -c 1,2 | rev)
+    
+    # date info
+    date=$(date "+%a, %d %b %Y")
+
+    #time info
+    time=$(date "+%T")
+
+    # battery info
+    batteryLevel=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "percentage: .+" | grep -Eo "[0-9]+%" | rev | cut -c 2- | rev)
+    batteryState=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "state: .+"      | grep -Eo "charging|discharging")
+
+    # battery block will appear only when battery is connected.
+    if [[ $batteryLevel != "" ]]
+    then
+       if (( $batteryLevel <= 100 ))
+       then
+           batteryIcon=""
+           batteryColour=$defaultColour
+       fi
+
+       if (( $batteryLevel <= 75 ))
+       then
+           batteryIcon=""
+           batteryColour=$defaultColour
+       fi
+       
+       if (( $batteryLevel <= 50 ))
+       then
+           batteryIcon=""
+           batteryColour=$defaultColour
+       fi
+    
+       if (( $batteryLevel <= 30 ))
+       then
+           batteryIcon=""
+           batteryColour=$bloodyRed
+       fi
+       
+       if (( $batteryLevel <= 10 ))
+       then
+           batteryIcon=""
+           batteryColour=$bloodyRed
+       fi
+       
+       batteryBlock=",{ \"name\"     :\"batteryInfo \"
+                     ,  \"color\"    :\"$batteryColour\"
+                     ,  \"full_text\":\"$batteryIcon  $batteryLevel% $batteryState\"}"
+       
+    else
+        batteryBlock=""
+    fi
+    
+    # audio info
+    audioState=$(amixer get Master  | grep -Eo "\[(off|on)\]" | sed -n '1,1 p')
+    masterLevel=$(amixer get Master | grep -Eo "[0-9]+%"      | sed -n '1,1 p')
+    if [[ $audioState == "[off]" ]]
+    then
+        audioIcon=""
+        audioColour=$bloodyRed
+    else
+        audioIcon=""
+        audioColour=$defaultColour
+    fi
+
+    # mpd info
+    nowPlaying=$(mpc current)
+
+    mpdStatus=$(mpc status | grep -Eo "(playing|paused)")
+    if [[ $mpdStatus == "playing" ]]
+    then
+        mpdIcon=""
+    elif [[ $mpdStatus == "paused" ]]
+    then
+        mpdIcon=""
+    else
+        mpdIcon=""
+    fi                    
+    
+    # connection info
+    wifiStatus=$(iw dev wlp2s0b1 link)
+    if [[ $wifiStatus != "Not connected." ]]
+    then
+        wlanIcon=""
+        wlanColor="$_wlanColor"
+        ssid=$(iw dev wlp2s0b1 link | grep -Eo "SSID: .+" | cut -c 7-)
+    else
+        wlanIcon=""
+        wlanColor=$bloodyRed
+        ssid="not connected"
+    fi                    
+
+    # output blocks
+        echo ",[{\"name\"     :\"mpd\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"$mpdIcon  $nowPlaying\"}
+                                        
+              , {\"name\"     :\"masterLevel \"
+              ,  \"color\"    :\"$audioColour\"
+              ,  \"full_text\":\"$audioIcon $masterLevel\"}
+                
+              , {\"name\"     :\"connectionInfo\"
+              ,  \"color\"    :\"$connectionColour\"
+              ,  \"full_text\":\"$connectionIcon  $connectionStatus\"}
+                
+              , {\"name\"     :\"keyboardInfo \"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"  $keyboardLayout\"}
+              
+              $batteryBlock
+                              
+              , {\"name\"     :\"date\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\"  $date\"}
+                
+              , {\"name\"     :\"time\"
+              ,  \"color\"    :\"$defaultColour\"
+              ,  \"full_text\":\" $time\"}
+           ]"
+    sleep 1
+done
